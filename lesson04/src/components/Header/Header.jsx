7a7c4fc5c2dc9a400943076f36e21cb7b544a2d3
AlexShep5 <alxshep25@gmail.com>
2022-12-14 21:49:18 +0300

    lesson04

---
+++ lesson04/src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import styles from './Header.module.scss'
+import { NavLink } from "react-router-dom";
+
+const links = [
+  {
+    id: 1,
+    name: 'Home',
+    to: '/'
+  },
+  {
+    id: 2,
+    name: 'Chats',
+    to: '/chats'
+  },
+  {
+    id: 3,
+    name: 'Profile',
+    to: '/profile'
+  }
+]
+
+export function Header(props) {
+  return (
+    <header>
+      <nav>
+        <ul className={`${styles.wrap} ${props.className}`}>
+          {links.map((item) => (
+            <li key={item.id}>
+              <NavLink style={({ isActive }) => ({
+                color: isActive ? 'green' : '#9fa8da'
+              })}
+                className={`${styles.link}`} to={item.to}>{item.name}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+
+  )
+}
